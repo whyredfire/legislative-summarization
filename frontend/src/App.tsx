276fabad12c700503ec455b88bb24f36de6fed2d
Karan Parashar <karan@pixelos.net>
2024-11-21 01:42:51 +0530

    Configure tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,8 @@
-import './App.css'
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-
-  return (
-    <>
-    </>
-  )
+  return <></>;
 }
 
-export default App
+export default App;
