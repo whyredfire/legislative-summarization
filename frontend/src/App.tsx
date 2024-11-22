f4895a0accb5beee0e8178b75af9f91fb7a3b5c5
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2024-11-22 12:02:55 +0545

    NavigationBar
    Change-Id: Iccada5fd73e738a242c73b373227dd30c1919683

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import "./App.css";
 import { Toaster } from "sonner";
 
 import SummarizeForm from "./components/SummarizeForm";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <>
       <Toaster richColors position="top-center" expand={true} />
+      <NavigationBar />
       <SummarizeForm />
     </>
   );
