0ff35c1e0e3efc340b3d7dd4f13dc4ad02d5062b
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-21 17:59:56 +0545

    feat(ui): Add footer section

---
+++ frontend/components/Footer/index.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="border-grid border-t py-6 md:py-0">
+        <div className="container-wrapper">
+          <div className="container text-center py-4">
+            Made with &#x2665;&#xfe0f; by Karan
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
