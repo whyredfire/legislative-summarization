39f7ece44e40830aec8101dbbeb012643d457e5e
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-21 19:36:05 +0545

    fix(typography): Accept props and custom classes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,11 @@
+import { H1, LeadingText } from "@/ui/Typography";
+
 const Home = () => {
   return (
     <>
-      <main className="flex flex-col items-center justify-items-center">
-        <h1>Hello World</h1>
+      <main className="text-center flex flex-col gap-4">
+        <H1>LegalEase</H1>
+        <LeadingText>Summarize all your legal stuffs</LeadingText>
       </main>
     </>
   );
--- frontend/ui/Typography.tsx
+++ frontend/ui/Typography.tsx
@@ -1,56 +1,124 @@
-export function H1({ children }: Readonly<{ children: React.ReactNode }>) {
+import { cn } from "@/lib/utils";
+
+export function H1({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"h1">) {
   return (
-    <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+    <h1
+      className={cn(
+        "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+        className
+      )}
+      {...props}
+    >
       {children}
     </h1>
   );
 }
 
-export function H2({ children }: Readonly<{ children: React.ReactNode }>) {
+export function H2({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"h2">) {
   return (
-    <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+    <h2
+      className={cn(
+        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+        className
+      )}
+      {...props}
+    >
       {children}
     </h2>
   );
 }
 
-export function H3({ children }: Readonly<{ children: React.ReactNode }>) {
+export function H3({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"h3">) {
   return (
-    <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+    <h3
+      className={cn(
+        "scroll-m-20 text-2xl font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    >
       {children}
     </h3>
   );
 }
 
-export function H4({ children }: Readonly<{ children: React.ReactNode }>) {
+export function H4({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"h4">) {
   return (
-    <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+    <h4
+      className={cn(
+        "scroll-m-20 text-xl font-semibold tracking-tight",
+        className
+      )}
+      {...props}
+    >
       {children}
     </h4>
   );
 }
 
-export function P({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>;
+export function P({
+  children,
+  className,
+  ...props
+}: React.ComponentProps<"p">) {
+  return (
+    <p
+      className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}
+      {...props}
+    >
+      {children}
+    </p>
+  );
 }
 
 export function Blockquote({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+  className,
+  ...props
+}: React.ComponentProps<"blockquote">) {
   return (
-    <blockquote className="mt-6 border-l-2 pl-6 italic">{children}</blockquote>
+    <blockquote
+      className={cn("mt-6 border-l-2 pl-6 italic", className)}
+      {...props}
+    >
+      {children}
+    </blockquote>
   );
 }
 
-export function List({ children }: Readonly<{ children: React.ReactNode }>) {
-  return <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>;
-}
+// export function List({ children }: Readonly<{ children: React.ReactNode }>) {
+//   return <ul className="my-6 ml-6 list-disc [&>li]:mt-2">{children}</ul>;
+// }
 
 export function InlineCode({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
+  className,
+  ...props
+}: React.ComponentProps<"code">) {
   return (
-    <code className="relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold">
+    <code
+      className={cn(
+        "relative rounded bg-muted px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
+        className
+      )}
+      {...props}
+    >
       {children}
     </code>
   );
@@ -58,24 +126,51 @@ export function InlineCode({
 
 export function LeadingText({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <p className="text-xl text-muted-foreground">{children}</p>;
+  className,
+  ...props
+}: React.ComponentProps<"p">) {
+  return (
+    <p className={cn("text-xl text-muted-foreground", className)} {...props}>
+      {children}
+    </p>
+  );
 }
 
 export function LargeText({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <div className="text-lg font-semibold">{children}</div>;
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div className={cn("text-lg font-semibold", className)} {...props}>
+      {children}
+    </div>
+  );
 }
 
 export function SmallText({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <small className="text-sm font-medium leading-none">{children}</small>;
+  className,
+  ...props
+}: React.ComponentProps<"small">) {
+  return (
+    <small
+      className={cn("text-sm font-medium leading-none", className)}
+      {...props}
+    >
+      {children}
+    </small>
+  );
 }
 
 export function MutedText({
   children,
-}: Readonly<{ children: React.ReactNode }>) {
-  return <p className="text-sm text-muted-foreground">{children}</p>;
+  className,
+  ...props
+}: React.ComponentProps<"p">) {
+  return (
+    <p className={cn("text-sm text-muted-foreground", className)} {...props}>
+      {children}
+    </p>
+  );
 }
