4845ef9ed3ad07adc9c96015bb528dc14c5ae424
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-21 18:02:21 +0545

    chore: Cleanup redundant divs

--- frontend/components/Footer/index.tsx
+++ frontend/components/Footer/index.tsx
@@ -2,10 +2,8 @@ const Footer = () => {
   return (
     <>
       <footer className="border-grid border-t py-6 md:py-0">
-        <div className="container-wrapper">
-          <div className="container text-center py-4">
-            Made with &#x2665;&#xfe0f; by Karan
-          </div>
+        <div className="container-wrapper container text-center py-4">
+          Made with &#x2665;&#xfe0f; by Karan
         </div>
       </footer>
     </>
--- frontend/components/SiteHeader/index.tsx
+++ frontend/components/SiteHeader/index.tsx
@@ -9,25 +9,23 @@ const SiteHeader = () => {
   return (
     <>
       <header className=" border-grid border-b-sidebar-border border">
-        <div className="container-wrapper">
-          <div className="container flex justify-between items-center p-4">
-            <H3>
-              <Link href={"/"}>LegalEase</Link>
-            </H3>
-            <div className="flex justify-between items-center gap-8">
-              <div className="flex space-x-4 items-center">
-                <Button variant={"ghost"}>
-                  <Link href={"/"}>Home</Link>
-                </Button>
-                <Button variant={"ghost"}>
-                  <Link href={"/register"}>Register</Link>
-                </Button>
-                <Button>
-                  <Link href={"/login"}>Login</Link>
-                </Button>
-              </div>
-              <ModeSwitcher />
+        <div className="container-wrapper container flex justify-between items-center p-4">
+          <H3>
+            <Link href={"/"}>LegalEase</Link>
+          </H3>
+          <div className="flex justify-between items-center gap-8">
+            <div className="flex space-x-4 items-center">
+              <Button variant={"ghost"}>
+                <Link href={"/"}>Home</Link>
+              </Button>
+              <Button variant={"ghost"}>
+                <Link href={"/register"}>Register</Link>
+              </Button>
+              <Button>
+                <Link href={"/login"}>Login</Link>
+              </Button>
             </div>
+            <ModeSwitcher />
           </div>
         </div>
       </header>
