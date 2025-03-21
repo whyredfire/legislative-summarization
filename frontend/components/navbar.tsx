75e8f385cffe29d96d025eb3fcaf41ea85067281
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-21 17:38:05 +0545

    chore(navbar): Move all links within button

--- frontend/components/navbar.tsx
+++ frontend/components/navbar.tsx
@@ -1,6 +1,7 @@
 import { ModeToggle } from "./themeswitch";
 import Link from "next/link";
 import { H3 } from "@/ui/Typography";
+import { Button } from "./ui/button";
 
 const NavigationBar = () => {
   return (
@@ -10,10 +11,16 @@ const NavigationBar = () => {
           <Link href={"/"}>LegalEase</Link>
         </H3>
         <div className="flex justify-between items-center gap-8">
-          <div className="flex space-x-4">
-            <Link href={"/"}>Home</Link>
-            <Link href={"/login"}>Login</Link>
-            <Link href={"/register"}>Register</Link>
+          <div className="flex space-x-4 items-center">
+            <Button variant={"ghost"}>
+              <Link href={"/"}>Home</Link>
+            </Button>
+            <Button variant={"ghost"}>
+              <Link href={"/register"}>Register</Link>
+            </Button>
+            <Button>
+              <Link href={"/login"}>Login</Link>
+            </Button>
           </div>
           <ModeToggle />
         </div>
