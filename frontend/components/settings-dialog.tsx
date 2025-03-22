d535783f4a70e8edc81a35126172cd2872757938
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-23 02:18:44 +0545

    feat: Add settings dialog page

---
+++ frontend/components/settings-dialog.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+interface SettingsDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export function SettingsDialog({ open, onOpenChange }: SettingsDialogProps) {
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent
+        className="sm:max-w-[425px]"
+        onPointerDownOutside={(e) => {
+          if (
+            e.target instanceof HTMLElement &&
+            e.target.closest('[role="dialog"]')
+          ) {
+            e.preventDefault();
+          }
+        }}
+      >
+        <DialogHeader>
+          <DialogTitle>Settings</DialogTitle>
+          <DialogDescription>
+            Make changes to your settings here.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogTitle>Delete account</DialogTitle>
+        <DialogDescription>
+          This action cannot be undone. This will permanently delete your
+          account and remove your data from our servers.
+        </DialogDescription>
+        <Button variant={"destructive"}>Delete</Button>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- frontend/components/user-dropdown.tsx
+++ frontend/components/user-dropdown.tsx
@@ -9,9 +9,11 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LogOut } from "lucide-react";
+import { LogOut, SettingsIcon } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
+import { SettingsDialog } from "./settings-dialog";
+import { useState } from "react";
 
 interface UserAvatarDropdownProps {
   username: string;
@@ -28,50 +30,71 @@ const UserAvatarDropdown = ({ username, email }: UserAvatarDropdownProps) => {
     navigator.refresh();
   };
 
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const handleOpenDialog = () => {
+    setDropdownOpen(false);
+    setTimeout(() => {
+      setDialogOpen(true);
+    }, 10);
+  };
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="outline"
-          className="size-8 rounded-full border-none p-0"
-        >
-          <Avatar>
-            <AvatarImage src="#!" alt={username} />
-            <AvatarFallback className="rounded-lg">
-              {username.charAt(0).toUpperCase()}
-            </AvatarFallback>
-          </Avatar>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent
-        sideOffset={20}
-        alignOffset={-150}
-        hideWhenDetached
-        align="start"
-      >
-        <DropdownMenuLabel className="p-0 font-normal">
-          <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+    <>
+      <DropdownMenu open={dropdownOpen} onOpenChange={setDropdownOpen}>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            className="size-8 rounded-full border-none p-0"
+          >
             <Avatar>
               <AvatarImage src="#!" alt={username} />
               <AvatarFallback className="rounded-lg">
                 {username.charAt(0).toUpperCase()}
               </AvatarFallback>
             </Avatar>
-            <div className="grid flex-1 text-left text-sm leading-tight">
-              <span className="truncate font-semibold">{username}</span>
-              <span className="text-muted-foreground truncate text-xs">
-                {email}
-              </span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          sideOffset={20}
+          alignOffset={-150}
+          hideWhenDetached
+          align="start"
+        >
+          <DropdownMenuLabel className="p-0 font-normal">
+            <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+              <Avatar>
+                <AvatarImage src="#!" alt={username} />
+                <AvatarFallback className="rounded-lg">
+                  {username.charAt(0).toUpperCase()}
+                </AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">{username}</span>
+                <span className="text-muted-foreground truncate text-xs">
+                  {email}
+                </span>
+              </div>
             </div>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={handleSignOut}>
-          <LogOut />
-          Sign Out
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            onSelect={(e) => {
+              e.preventDefault();
+              handleOpenDialog();
+            }}
+          >
+            <SettingsIcon />
+            Settings
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onClick={handleSignOut}>
+            <LogOut />
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <SettingsDialog open={dialogOpen} onOpenChange={setDialogOpen} />
+    </>
   );
 };
 
