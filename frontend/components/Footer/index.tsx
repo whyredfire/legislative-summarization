ca20e8954b5a7290d20bfcb1e0249cb361ed3dee
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-22 21:01:53 +0545

    chore(footer): Add copyrights

--- frontend/components/Footer/index.tsx
+++ frontend/components/Footer/index.tsx
@@ -1,9 +1,15 @@
+import { MutedText, P } from "../ui/typography";
+
 const Footer = () => {
   return (
     <>
       <footer className="border-grid border-t py-6 md:py-0">
         <div className="container-wrapper container text-center py-4">
-          Made with &#x2665;&#xfe0f; by anonymous
+          <P>Made with &#x2665;&#xfe0f; by Team Legislative Summarization</P>
+          <MutedText>
+            &copy; Copyright 2025 - {new Date().getFullYear()}. All Rights
+            Reserved.
+          </MutedText>
         </div>
       </footer>
     </>
