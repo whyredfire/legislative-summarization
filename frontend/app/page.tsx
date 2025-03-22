54e99ddd2a22490ba93d4dc2cec56dacf0afe8b3
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-22 08:16:20 +0545

    feat: Add summarization page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { H1, LeadingText } from "@/ui/Typography";
+import Link from "next/link";
 
 const Home = () => {
   return (
@@ -6,6 +8,9 @@ const Home = () => {
       <main className="text-center flex flex-col gap-4">
         <H1>LegalEase</H1>
         <LeadingText>Summarize all your legal stuffs</LeadingText>
+        <Button className="rounded-full mx-auto cursor-pointer">
+          <Link href={"/summarize"}>Click here to start</Link>
+        </Button>
       </main>
     </>
   );
---
+++ frontend/app/summarize/page.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const SummarizePage = () => {
+  const formSchema = z.object({
+    summarizeText: z.string().min(15),
+    summarizedText: z.string(),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      summarizeText: "",
+      summarizedText: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="grid grid-cols-1 sm:grid-cols-2 flex-wrap gap-4 w-full"
+        >
+          <FormField
+            control={form.control}
+            name="summarizeText"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    autoCorrect="off"
+                    autoCapitalize="off"
+                    spellCheck="false"
+                    className="min-h-[30vh] max-h-[30vh] resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="summarizedText"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    readOnly
+                    className="min-h-[30vh] resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="col-span-1 sm:col-span-2 flex justify-center">
+            <Button className="mt-4 mx-auto rounded-full" type="submit">
+              Summarize
+            </Button>
+          </div>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default SummarizePage;
