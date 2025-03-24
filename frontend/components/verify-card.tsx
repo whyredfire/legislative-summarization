ad502133f7307d4984826c2ea9a8539f5a391d40
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-03-24 13:57:59 +0545

    fix(register-verify): Add resend OTP feature
    it takes uniqueId and sends OTP there

--- frontend/components/verify-card.tsx
+++ frontend/components/verify-card.tsx
@@ -59,6 +59,19 @@ const VerifyCard = ({ uniqueId }: { uniqueId: string }) => {
     }
   };
 
+  const resendOTP = async () => {
+    try {
+      const response = await api.post("/auth/register/resend", {
+        uniqueId: uniqueId,
+      });
+      if (response.status === 200) {
+        toast.success("OTP sent successfully.");
+      }
+    } catch (error) {
+      toast.error("Error while resending OTP");
+    }
+  };
+
   return (
     <>
       <Card className="p-4">
@@ -69,35 +82,43 @@ const VerifyCard = ({ uniqueId }: { uniqueId: string }) => {
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="otp"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <InputOTP maxLength={6} {...field}>
-                      <InputOTPGroup>
-                        <InputOTPSlot index={0} />
-                        <InputOTPSlot index={1} />
-                        <InputOTPSlot index={2} />
-                      </InputOTPGroup>
-                      <InputOTPSeparator />
-                      <InputOTPGroup>
-                        <InputOTPSlot index={3} />
-                        <InputOTPSlot index={4} />
-                        <InputOTPSlot index={5} />
-                      </InputOTPGroup>
-                    </InputOTP>
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex flex-col">
-              <Button className="mx-auto mt-4" type="submit">
-                Verify
+            <div className="flex flex-col gap-4">
+              <FormField
+                control={form.control}
+                name="otp"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <InputOTP maxLength={6} {...field}>
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                          <InputOTPSlot index={1} />
+                          <InputOTPSlot index={2} />
+                        </InputOTPGroup>
+                        <InputOTPSeparator />
+                        <InputOTPGroup>
+                          <InputOTPSlot index={3} />
+                          <InputOTPSlot index={4} />
+                          <InputOTPSlot index={5} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button
+                type="button"
+                onClick={resendOTP}
+                className="mx-auto"
+                variant={"secondary"}
+              >
+                Resend OTP
               </Button>
             </div>
+            <Button className="w-full mt-4" type="submit">
+              Verify
+            </Button>
           </form>
         </Form>
       </Card>
