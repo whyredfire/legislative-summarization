a1eb595094a13fe6683aee918600ab79e8b29fc6
Karan Parashar <karan@pixelos.net>
2025-04-23 13:31:56 +0530

    chore(next): Rebrand for legislative summarization

--- next-server/components/footer.tsx
+++ next-server/components/footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <footer className="border-grid border-t py-6 md:py-0">
         <div className="container-wrapper container text-center py-4">
           <SmallText className="font-md">
-            Made with &#x2665;&#xfe0f; by Team LegalEase
+            Made with &#x2665;&#xfe0f; by Team Legislative Summarization
           </SmallText>
           <MutedText className="mt-2 font-sm">
             &copy; Copyright {new Date().getFullYear()}. All Rights Reserved.
--- next-server/data/globals.ts
+++ next-server/data/globals.ts
@@ -1,5 +1,5 @@
 const siteConfig = {
-  title: "LegalEase",
+  title: "Legislative Summarization",
   description:
     "Summarize legal documents with the help of AI, and save time and effort.",
 } as const;
