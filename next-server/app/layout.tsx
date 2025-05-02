6a9f894d88b641497aab53f312104fdc5f58157f
Hridaya Prajapati <info.hridayaprajapati@gmail.com>
2025-05-02 15:20:08 +0545

    feat(next-server): Add ViewTransition

--- next-server/app/layout.tsx
+++ next-server/app/layout.tsx
@@ -9,6 +9,8 @@ import { Toaster } from "@/components/ui/sonner";
 import { siteConfig } from "@/data/globals";
 import { ThemeProvider } from "@/context/theme-provider";
 
+import { unstable_ViewTransition as ViewTransition } from "react";
+
 export const metadata: Metadata = {
   title: siteConfig.title,
   description: siteConfig.description,
@@ -24,16 +26,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex min-h-screen flex-col">
-          <ThemeProvider>
-            <SiteHeader />
-            <div className="container-wrapper container flex flex-1 items-center justify-center">
-              {children}
-            </div>
-            <Footer />
-            <Toaster richColors position="top-center" />
-          </ThemeProvider>
-        </div>
+        <ViewTransition>
+          <div className="flex min-h-screen flex-col">
+            <ThemeProvider>
+              <SiteHeader />
+              <div className="container-wrapper container flex flex-1 items-center justify-center">
+                {children}
+              </div>
+              <Footer />
+              <Toaster richColors position="top-center" />
+            </ThemeProvider>
+          </div>
+        </ViewTransition>
       </body>
     </html>
   );
